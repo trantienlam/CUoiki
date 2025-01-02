4e475841d181d659531d8d54cfab1667eb2cb5bb
trantienlam <lamvuc111@gmail.com>
2025-01-02 16:25:09 +0700

    second

--- controllers/admin/product.controllers.js
+++ controllers/admin/product.controllers.js
@@ -1,6 +1,58 @@
+const Product = require("../../models/product.model");
+
 // [GET] /admin/products
-module.exports.index = (req, res) => {
+module.exports.index = async (req, res) => {
+  let filterStatus = [
+    {
+      name: "Tất cả",
+      status: "",
+      class: "",
+    },
+    {
+      name: "Hoạt động",
+      status: "active",
+      class: "",
+    },
+    {
+      name: "Dừng hoạt động",
+      status: "inactive",
+      class: "",
+    },
+  ];
+
+  if (req.query.status) {
+    const index = filterStatus.findIndex(
+      (item) => item.status == req.query.status
+    );
+    filterStatus[index].class = "active";
+  } else {
+    const index = filterStatus.findIndex((item) => item.status == "");
+    filterStatus[index].class = "active";
+  }
+
+  //console.log(req.query.status);
+  let find = {
+    deleted: false,
+  };
+
+  if (req.query.status) {
+    find.status = req.query.status;
+  }
+
+  let keyword = "";
+
+  if (req.query.keyword) {
+    keyword = req.query.keyword;
+    const regex = new RegExp(keyword, "i");
+    find.title = regex;
+  }
+
+  const products = await Product.find(find);
+
   res.render("admin/pages/products/index", {
-    pageTitle: "Trang danh sách sản phẩm",
+    pageTitle: "Danh sách sản phẩm",
+    products: products,
+    filterStatus: filterStatus,
+    keyword: keyword,
   });
 };
